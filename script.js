fde49285e0d03e59e70b43959f24cc5b0679eb8e
cclund444 <87826114+cclund444@users.noreply.github.com>
2021-11-16 21:33:33 -0700

    17.3 practice code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { number } = require("yargs");
+
 const containerEl = document.getElementById('container');
 
 const clickHandler = function(event) {
@@ -5,5 +7,27 @@ const clickHandler = function(event) {
     event.target.textContent = 'Clicked!';
   }
 };
+
+const data  = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  if (num === arr[middle]){
+    return 'found it';
+  }
+  else if (num < arr[middle]) {
+    right = middle;
+    return `bring right down to ${right}`;
+  }
+  else if (num > arr[middle]) {
+    right = middle + 1;
+    return `bring left up to ${left}`;
+  }
+};
+
+console.log(binarySearch(data, 38));
 
 containerEl.addEventListener('click', clickHandler);
